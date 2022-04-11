e9e5a2b09735fb721e50e4b316938b0662932dc1
asharma13524 <asharma13524@gmail.com>
2022-04-10 20:16:19 -0400

    IERC1155

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
-interface IERC20 {
+interface ERC1155 {
     function transfer(address to, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
